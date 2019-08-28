84237405a45e0c82c48925638c97bcb501a406a4
Pogba Ye <pogba.ye@cargosmart.com>
2019-08-28 17:11:42 +0800

    add center layout

--- 最后的面试准备CSS基础部分/CSS.基础.js
+++ 最后的面试准备CSS基础部分/CSS.基础.js
@@ -1,1 +1,20 @@
 https://mp.weixin.qq.com/s/RT6YgM6a4rWj8Qj47DkeHA
+
+box.clientHeight: content + padding
+box.offsetHeight: content + padding + border
+
+//如果box的父容器没有设置定位，则box.offsetLeft是相对body而言，否则是根据离它最近且有设置定位的父容器而言
+box.offsetLeft //获取box盒子相对它的父容器水平的偏移
+box.offsetTop //获取box盒子相对它的父容器垂直的偏移
+
+
+
+
+
+
+
+
+
+
+
+
